0f3c223f901aee176bdcdf2e44640a40d05d159e
Stephanieperl <stephanieperla2286@gmail.com>
2020-10-26 21:58:43 -0600

    Agregando saltos de linea

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
-    echo "Primera prueba ultilizando GitHub";
-    echo "Hola Mundo";
+    echo "Primera prueba ultilizando GitHub <br>";
+    
+    echo "Hola Mundo <br>";
+
+    echo "Primer cometario por Stephanie Perla <br>"
 ?>
--- integrantes.php
+++ integrantes.php
@@ -1,13 +1,15 @@
 <?php 
   
-  echo "Este es mi grupo";
-  echo "Omar Andrade - u20140319";
+  echo "Este es mi grupo <br>";
 
-  echo "Dilcia Munguia - u20140101";
+
+  echo "Omar Andrade - u20140319 <br>";
+
+  echo "Dilcia Munguia - u20140101 <br>";
  
-  echo "Stephanie Perla - u20061136"; 
+  echo "Stephanie Perla - u20061136 <br>"; 
 
-  echo "Virginia Escobar - u20140348"; 
+  echo "Virginia Escobar - u20140348 <br>"; 
   
 
 ?>
